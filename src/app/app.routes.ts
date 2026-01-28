2440bed56f1b9eaf6e2880b10e0b8bf6d6eb3fba
adminhype <ado.designs@outlook.de>
2026-01-28 12:13:42 +0100

    feat: prepare project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { LandingComponent } from './features/landing/features/auth/landing/landing.component';
+import { VideoPlayerComponent } from './features/video-player/video-player.component';
+import { ImprintComponent } from './features/legal/imprint/imprint.component';
+import { PrivacyComponent } from './features/legal/privacy/privacy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'browse', component: DashboardComponent },
+    { path: 'watch/:id', component: VideoPlayerComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy', component: PrivacyComponent },
+    { path: '**', redirectTo: '' }
+];
