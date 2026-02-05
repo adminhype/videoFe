2221b5629ca16c4d1b1825c98b7eee0727ae73fb
adminhype <ado.designs@outlook.de>
2026-02-05 18:06:50 +0100

    feat: legal pages and global scroll

--- src/app/features/legal/imprint/imprint.component.ts
+++ src/app/features/legal/imprint/imprint.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
 
 @Component({
   selector: 'app-imprint',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
+  constructor(private location: Location) {}
 
+  goBack() {
+    this.location.back();
+  }
 }
--- src/app/features/legal/privacy/privacy.component.ts
+++ src/app/features/legal/privacy/privacy.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
 
 @Component({
   selector: 'app-privacy',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './privacy.component.html',
   styleUrl: './privacy.component.scss'
 })
 export class PrivacyComponent {
+  constructor(private location: Location) {}
 
+  goBack() {
+    this.location.back();
+  }
 }
